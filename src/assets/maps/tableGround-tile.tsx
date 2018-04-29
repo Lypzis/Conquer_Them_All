1485ca90eb937e9b13b19db2b184d09bef0f2a46
Lypzis <victorvp2008@hotmail.com>
2018-04-29 17:06:53 -0300

    minor update

--- src/assets/maps/tableGround-tile.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tableGround-tile" tilewidth="32" tileheight="32" tilecount="2" columns="2">
- <image source="../images/tableGround-tile.png" width="64" height="32"/>
-</tileset>
